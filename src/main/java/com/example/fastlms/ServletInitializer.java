72bb1f89b61e444fc4018aff4678807b19afda28
JJH <gksqlcxkdns1@naver.com>
2022-04-17 21:09:07 +0900

    fastlms 초기화 설정

--- src/main/java/com/example/fastlms/ServletInitializer.java
+++ src/main/java/com/example/fastlms/ServletInitializer.java
@@ -5,6 +5,7 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 
 public class ServletInitializer extends SpringBootServletInitializer {
 
+    //2022.04.17
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(FastlmsApplication.class);
